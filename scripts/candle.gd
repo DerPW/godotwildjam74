fe31fb94cee47aa99d8806cbde6798dc92be3bd8
Moritz Zier <moritz.zier@web.de>
2024-10-15 23:09:33 +0200

    changed candle behavior

--- scripts/candle.gd
+++ scripts/candle.gd
@@ -2,6 +2,10 @@ extends PointLight2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+@onready var tween: Tween = get_tree().create_tween()
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animation_player.play("flicker")
+	tween.tween_property(self, ^"energy", 1.0, .5)
+	tween.tween_property(self, ^"energy", .6, .2)
+	tween.set_loops()
+	
