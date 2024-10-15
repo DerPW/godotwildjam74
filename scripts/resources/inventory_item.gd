51bd309c5c541f946961577def90556a605ff94b
Moritz Zier <moritz.zier@web.de>
2024-10-15 23:00:30 +0200

    added hud and new tiles

--- scripts/resources/inventory_item.gd
+++ scripts/resources/inventory_item.gd
@@ -3,7 +3,9 @@ class_name InventoryItem
 extends Resource
 
 enum ITEM_TYPE {
-	KEY_IRON
+	KEY_IRON,
+	KEY_GOLD,
+	KEY_PLATINUM
 }
 
 @export var item_type: ITEM_TYPE
--- scripts/ui/inventory_items.gd
+++ scripts/ui/inventory_items.gd
@@ -4,8 +4,15 @@ extends Control
 
 
 func _ready() -> void:
-	pass
+	Inventory.items_changed.connect(update_inventory_items)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func update_inventory_items(items: Array[InventoryItem]) -> void:
+	for child in get_children():
+		remove_child(child)
+		
+	for item in items:
+		var elem = TextureRect.new()
+		elem.texture = item.sprite
+		add_child(elem)
+		
+		
