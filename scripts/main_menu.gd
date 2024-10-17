c321457efcd7d2e1cda84ebf49a1181234414328
DerPW <p.weidner.p@googlemail.com>
2024-10-18 00:34:31 +0200

    menu to main scene transition (with flashing light 😡)

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -11,4 +11,9 @@ func _load_main_scene() -> void:
 
 
 func _on_button_pressed() -> void:
+	var circle := $CanvasLayer/TransitionCircle.material as ShaderMaterial
+	var tween := get_tree().create_tween()
+	tween.tween_property(circle, "shader_parameter/circle_size", 0, 1)
+	# wait for circle to close
+	await get_tree().create_timer(1).timeout
 	_load_main_scene()
