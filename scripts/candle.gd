c3db808ac9371e32a2a88724baf9fbb9a51c72e7
Moritz Zier <moritz.zier@web.de>
2024-10-18 17:30:49 +0200

    fixed tweening bugs

--- scripts/candle.gd
+++ scripts/candle.gd
@@ -1,9 +1,9 @@
 extends PointLight2D
 
-@onready var tween: Tween = get_tree().create_tween()
+@onready var tween: Tween = self.create_tween()
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	tween.tween_property(self, ^"energy", 1.0, .5)
+	tween.tween_property(self, ^"energy", 1.0, 2)
 	tween.tween_property(self, ^"energy", .6, .2)
 	tween.set_loops()
 	
--- scripts/collection_zone.gd
+++ scripts/collection_zone.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	area_entered.connect(_on_area_enter)
 
 
-func _on_area_enter(area: Pickup) -> void:
+func _on_area_enter(area: Area2D) -> void:
 	if area is not Pickup:
 		return
 		
