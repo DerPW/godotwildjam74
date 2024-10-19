5cdefb6bd2e7ee42f8420d3da5e52c48e0e0da4a
DerPW <p.weidner.p@googlemail.com>
2024-10-19 18:19:55 +0200

    level 5 and switches can now ad and move tiles

--- scripts/switch.gd
+++ scripts/switch.gd
@@ -4,7 +4,8 @@ extends Node2D
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 @export var walls_tilemap_layer : TileMapLayer
-@export var switchable_walls_coordinates: Array[Vector2i]
+@export var wall_to_add: Array[Vector2i]
+@export var wall_to_remove: Array[Vector2i]
 
 func _ready() -> void:
 	pass
@@ -19,8 +20,9 @@ func _activate_switch(value : bool) -> void:
 	if not value and activated: return
 	sprite_2d.region_rect = Rect2(16, 0, 16, 16)
 	audio_stream_player.play()
-	walls_tilemap_layer.set_cells_terrain_connect(switchable_walls_coordinates, 0, -1)
-
+	
+	walls_tilemap_layer.set_cells_terrain_connect(wall_to_add, 0, 0)
+	walls_tilemap_layer.set_cells_terrain_connect(wall_to_remove, 0, -1)
 
 func _on_activation_area_body_entered(body: Node2D) -> void:
 	if not body is God or activated: return 
